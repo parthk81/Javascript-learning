0b541b56376f250a5d01a8a8d9a1db7a40cd8d2c
Parth Kalavdiya <163581184+parthk81@users.noreply.github.com>
2024-04-30 06:44:30 +0000

    First commite

---
+++ learnings/Variable.js
@@ -0,0 +1,16 @@
+const accountId = 11845
+let accountEmail = "parthk822@gmail.com"
+var accountPassword ="15684"
+accountCity ="surat"
+
+// accountId = 2
+ 
+accountEmail = "hr@gmail.com"
+accountPassword = "415871"
+accountCity = "navsari"
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
